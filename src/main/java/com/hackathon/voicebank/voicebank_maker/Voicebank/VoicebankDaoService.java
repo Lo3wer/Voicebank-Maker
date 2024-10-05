5163839412cabe67014efeac0323d7ced15de4e6
Yandrew26 <andrewyan0226@gmail.com>
2024-10-05 16:47:59 -0700

    backend - GET requests for voicebank per id

--- src/main/java/com/hackathon/voicebank/voicebank_maker/Voicebank/VoicebankDaoService.java
+++ src/main/java/com/hackathon/voicebank/voicebank_maker/Voicebank/VoicebankDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class VoicebankDaoService {
@@ -13,9 +14,16 @@ public class VoicebankDaoService {
 
     static {
         voicebanks.add(new Voicebank(1, "recording 1", new File("rec1.wav")));
+        voicebanks.add(new Voicebank(2, "recording 2", new File("rec2.wav")));
+        voicebanks.add(new Voicebank(3, "recording 3", new File("rec3.wav")));
     }
 
     public List<Voicebank> getVoicebanks() {
         return voicebanks;
+    }
+
+    public Voicebank getVoicebank(int id) {
+        Predicate<? super Voicebank> predicate = voicebank -> voicebank.getId().equals(id);
+        return voicebanks.stream().filter(predicate).findFirst().get();
     }
 }
--- src/main/java/com/hackathon/voicebank/voicebank_maker/Voicebank/VoicebankResource.java
+++ src/main/java/com/hackathon/voicebank/voicebank_maker/Voicebank/VoicebankResource.java
@@ -1,6 +1,7 @@
 package com.hackathon.voicebank.voicebank_maker.Voicebank;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,8 +15,15 @@ public class VoicebankResource {
         this.service = service;
     }
 
+    // GET voicebanks
     @GetMapping("/voicebanks")
     public List<Voicebank> getAllVoicebanks() {
         return service.getVoicebanks();
+    }
+
+    //GET id of voicebank
+    @GetMapping("/voicebanks/{id}")
+    public Voicebank getVoicebank(@PathVariable int id) {
+        return service.getVoicebank(id);
     }
 }
