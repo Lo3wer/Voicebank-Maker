ee4c5ca317c8f98816d7e4e1483a9cb70b1ef334
Yoyoma2000 <alanqq920@gmail.com>
2024-10-06 11:16:16 -0700

    Formatting

--- frontend/src/pages/RecordingMenu.js
+++ frontend/src/pages/RecordingMenu.js
@@ -5,7 +5,7 @@ function recordingMenu() {
 
     return (
         <div className="flex flex-col justify-center align-middle items-center h-screen w-screen bg-white bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white p-8">
-                <h1 className="text-2xl p-5 font-bold text-white">
+                <h1 className="text-4xl font-bold mb-6 text-white">
                     Sounds to record:
                 </h1>
 
