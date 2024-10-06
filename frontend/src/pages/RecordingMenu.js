dc94e29459a65f78061caa12aef74aeaf610939e
Yoyoma2000 <alanqq920@gmail.com>
2024-10-06 01:02:24 -0700

    Updated recorder with next button

--- frontend/src/pages/RecordingMenu.js
+++ frontend/src/pages/RecordingMenu.js
@@ -5,12 +5,12 @@ function recordingMenu() {
 
     return (
         <div className="flex flex-col justify-center align-middle items-center h-screen w-screen bg-white">
-                <h1 className="">
-                    Record
+                <h1 className="text-2xl p-5 font-bold text-gray-600">
+                    Sounds to record:
                 </h1>
-                <div className={"h-[100vh] pt-10"}>
-                    <ReactRecorder/>
-                </div>
+            <div className={"h-[100vh] pt-10"}>
+                    <ReactRecorder />
+            </div>
         </div>
     )
 }
